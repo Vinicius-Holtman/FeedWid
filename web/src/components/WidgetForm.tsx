e12b934557b58b03f99b6189be5b20db3b2c28ce
Vinicius <viniholtman28@gmail.com>
2022-05-03 10:30:22 -0300

    Finished component WidgetForm and CloseButton

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,6 +1,41 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageUrl from '../assets/bug.svg'
+import ideaImageUrl from '../assets/idea.svg'
+import thoughtImageUrl from '../assets/thought.svg'
 
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Iamgem de um balão de pensamento'
+    }
+  }
+}
+
+// Object.entries(feedbackTypes) => 
+/**
+ * [
+ *    ['BUG', {...}]
+ *    ['IDEIA', {...}]
+ *    ['OTHER', {...}]
+ * ]
+ */
 
 export function WidgetForm() {
   return (
@@ -10,6 +45,21 @@ export function WidgetForm() {
 
         <CloseButton />
       </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              // onClick={}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          )
+        })}
+      </div>
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela Rocketseat <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
